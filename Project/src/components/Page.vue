fd40a649aeaf3a47ef8fa2390e3870680542f42a
Devvaru <syeraru@gmail.com>
2025-07-03 19:34:43 -0600

    Set up files and page background

---
+++ Project/src/components/Page.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+    <div id="container">
+
+    </div>
+</template>
+
+<style scoped>
+#container {
+    background: #41445A;
+    box-shadow: 30px 30px 80px -7px #161B23;
+    border-radius: 42px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 50px 100px 50px 100px;
+}
+</style>
